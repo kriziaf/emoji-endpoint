dd950253ffd9d1105d89f9867619152fe3256c2f
cfernando415 <ca.fernando415@gmail.com>
2019-04-27 14:23:26 -0400

    moved random emoji selection to backend to reduce network payload

--- app/controllers/api/v1/emoji_controller.rb
+++ app/controllers/api/v1/emoji_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::EmojiController < ApplicationController
     def index
         @emoji = Emoji.all
+
+        render json: @emoji[rand(@emoji.length)]
+    end
+
+    def show
+        @emoji = Emoji.find(params[:id])
         render json: @emoji
     end
 end
